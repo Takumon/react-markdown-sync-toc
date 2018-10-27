8039a852a3cca467833717390dfddd4390a5289f
takumon <inouetakumon@gmail.com>
2018-10-27 19:36:45 +0900

    Updates

---
+++ precache-manifest.47adfa2a9a92c80e3cfa0f8dc541bd4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "78c03808a9b4869918d2",
+    "url": "/static/js/main.78c03808.chunk.js"
+  },
+  {
+    "revision": "6bfbb064c2509a85a657",
+    "url": "/static/js/1.6bfbb064.chunk.js"
+  },
+  {
+    "revision": "78c03808a9b4869918d2",
+    "url": "/static/css/main.3b1a274c.chunk.css"
+  },
+  {
+    "revision": "2972ccc8a474c3ddc26a44a20c2ce547",
+    "url": "/index.html"
+  }
+];
